ee3a295f326f606bd91d33b6281c49cc3896197e
malinborn <rikkeisy@gmail.com>
2023-12-03 10:15:35 +0300

    Added Finance LOA handling

--- main.py
+++ main.py
@@ -111,6 +111,9 @@ def loa_handler_general(assets: list[Asset]) -> list[Asset]:
     return resulting
 
 
+# TODO: Вот тут нужно зарефакторить, есть прикольная идея — переделать все эвристики на лямбды/функции.
+#       Присвоить их в переменные и вызывать уже их. По сути, бахнуть в делегаты и дать возможность
+#       переназначать лямбду на
 def loa_handler_engineering(assets: list[Asset]) -> list[Asset]:
     resulting: list[Asset] = list()
     added_names = list()
@@ -165,6 +168,60 @@ def loa_handler_engineering(assets: list[Asset]) -> list[Asset]:
     return resulting
 
 
+def loa_handler_finance(assets: list[Asset]) -> list[Asset]:
+    resulting: list[Asset] = list()
+    added_names = list()
+
+    finance_units = []
+    finance_depos = ["Finance"]
+
+    assets.sort(key=lambda x: x.name)
+
+    for asset in assets:
+        if asset.department not in finance_depos and asset.unit not in finance_units:
+            continue
+        if asset.name.strip() not in added_names:
+            resulting.append(asset)
+            added_names.append(asset.name.strip())
+        else:
+            asset_index = added_names.index(asset.name)
+
+            if asset.is_in_pci_dss_scope:
+                resulting[asset_index].is_in_pci_dss_scope = True
+
+            if asset.has_iid_clients:
+                resulting[asset_index].has_iid_clients = True
+
+            if asset.has_iid_employees:
+                resulting[asset_index].has_iid_employees = True
+
+            if asset.sensitive_data_description != resulting[
+                asset_index].sensitive_data_description and asset.sensitive_data_description != "":
+                resulting[asset_index].sensitive_data_description.extend(asset.sensitive_data_description)
+
+            if asset.business_owner != resulting[asset_index].business_owner and asset.business_owner != "":
+                resulting[asset_index].business_owner.extend(asset.business_owner)
+
+            if asset.purpose != resulting[asset_index].purpose and asset.purpose != "":
+                resulting[asset_index].purpose.extend(asset.purpose)
+
+            if asset.access_owner != resulting[asset_index].access_owner and asset.access_owner != "":
+                resulting[asset_index].access_owner.extend(asset.access_owner)
+
+            if asset.comments != resulting[asset_index].comments and asset.comments != "":
+                resulting[asset_index].comments.extend(asset.comments)
+
+            resulting[asset_index].department_and_unit = (resulting[asset_index].department_and_unit.strip()
+                                                          + "; " + asset.department_and_unit.strip())
+    logger.debug("collapsing doubling entities...")
+
+    for asset in resulting:
+        asset.collapse()
+    logger.debug("doubling entities collapsed")
+
+    return resulting
+
+
 def prepare_data(assets: list[Asset]) -> list[Asset]:
     match LOA_OPTION:
         case LOA_OPTION.GENERAL:
@@ -174,7 +231,7 @@ def prepare_data(assets: list[Asset]) -> list[Asset]:
         case LOA_OPTION.GDPR:
             raise NotImplemented
         case LOA_OPTION.FINANCE:
-            raise NotImplemented
+            return loa_handler_finance(assets)
         case _:
             raise NotImplemented
 
@@ -268,7 +325,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to compute LOA, check README")
     parser.add_argument("--cache", type=str, help="If you want to load from cache, pass \"True\"")
     parser.add_argument("--domain", type=str, help="Enter name of domain for which you want to collect LOA, "
-                                                   "for example, \"GDPR\", \"DE\", etc. GENERAL is default value. "
+                                                   "for example, \"GDPR\", \"DE\", \"Finance\" etc. GENERAL is default value. "
                                                    "Make sure that you configured link to spreadsheet in config.jsom")
     args = parser.parse_args()
 
@@ -277,7 +334,9 @@ if __name__ == "__main__":
     match args.domain.upper():
         case "DE" | "ENGINEERING":
             LOA_OPTION = LoaOptions.ENGINEERING
+        case "FN" | "FINANCE":
+            LOA_OPTION = LoaOptions.FINANCE
         case _:
             LOA_OPTION = LoaOptions.GENERAL
-
+    logger.info(f"{LOA_OPTION=}")
     main(cache_option)
