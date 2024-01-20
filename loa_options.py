b1f3b0fa6912e6c9d38a92a82c5a329352db7632
malinborn <rikkeisy@gmail.com>
2024-01-20 14:48:41 +0300

    Added domain all variables to Enum

--- loa_options.py
+++ loa_options.py
@@ -6,3 +6,11 @@ class LoaOptions(Enum):
     GDPR = 2
     FINANCE = 3
     ENGINEERING = 4
+    CORPORATE = 5
+    DODO_PIZZA_EURASIA = 6
+    MARKETING = 7
+    DRINKIT = 8
+    KEBSTER = 9
+    DRINKIT_UAE = 10
+    DODO_PIZZA_UAE = 11
+    MENUSA = 12
